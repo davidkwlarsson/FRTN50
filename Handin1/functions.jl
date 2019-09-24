4ec671d7ece94469cb2776b41046f2b9256bea0e
davidkwlarsson <dkw.larsson@gmail.com>
2019-09-24 14:47:25 +0200

    Add handin1 folder

---
+++ Handin1/functions.jl
@@ -0,0 +1,145 @@
+"""
+    quad(x,Q,q)
+
+Compute the quadratic
+
+	1/2 x'Qx + q'x
+
+"""
+function quad(x,Q,q)
+	return 1/2 * transpose(x)*Q*x + transpose(q)*x
+end
+
+
+
+"""
+    guadconj(y,Q,q)
+
+Compute the convex conjugate of the quadratic
+
+	1/2 x'Qx + q'x
+
+"""
+function quadconj(y,Q,q)
+	return 1/2*transpose(y-q)*inv(Q)*(y-q)
+
+end
+
+
+
+"""
+    box(x,a,b)
+
+Compute the indicator function of for the box contraint
+
+	a <= x <= b
+
+where the inequalites are applied element-wise.
+"""
+function box(x,a,b)
+	return all(a .<= x .<= b) ? 0.0 : Inf
+end
+
+
+
+"""
+    boxconj(y,a,b)
+
+Compute the convex conjugate of the indicator function of for the box contraint
+
+	a <= x <= b
+
+where the inequalites are applied element-wise.
+"""
+function boxconj(y,a,b)
+	box_conj = 0
+	for i=1:length(y)
+		if (y[i] < 0)
+			box_conj += a[i]*y[i]
+		else
+			box_conj += b[i]*y[i]
+		end
+	end
+	return box_conj
+end
+
+
+
+"""
+    grad_quad(x,Q,q)
+
+Compute the gradient of the quadratic
+
+	1/2 x'Qx + q'x
+
+"""
+function grad_quad(x,Q,q)
+	return Q*x .+ q
+end
+
+
+
+"""
+    grad_quadconj(y,Q,q)
+
+Compute the gradient of the convex conjugate of the quadratic
+
+	1/2 x'Qx + q'x
+
+"""
+function grad_quadconj(y,Q,q)
+	return inv(Q)*(y.-q)
+end
+
+
+
+"""
+    prox_box(x,a,b)
+
+Compute the proximal operator of the indicator function for the box contraint
+
+	a <= x <= b
+
+where the inequalites are applied element-wise.
+"""
+function prox_box(x,a,b,gamma)
+	proxbox = zeros(length(x))
+	for index in eachindex(x)
+
+		if b[index] < x[index]
+			proxbox[index] = b[index]
+		elseif x[index] < a[index]
+			proxbox[index] = a[index]
+		else
+			proxbox[index] = x[index]
+		end
+	end
+	return proxbox
+end
+
+
+
+"""
+    prox_boxconj(y,a,b)
+
+Compute the proximal operator of the convex conjugate of the indicator function
+for the box contraint
+
+	a <= x <= b
+
+where the inequalites are applied element-wise.
+"""
+function prox_boxconj(y,a,b,gamma)
+	return (y - gamma*prox_box(y/gamma,a,b,1/gamma))
+end
+
+
+"""
+    dual2primal(y,Q,q,a,b)
+
+Computes the solution to the primal problem for Hand-In 1 given a solution y to
+the dual problem.
+"""
+function dual2primal(y,Q,q,a,b)
+	return grad_quadconj(y,Q,q)
+end
---
+++ Handin1/prox_grad.jl
@@ -0,0 +1,82 @@
+using LinearAlgebra, Plots
+
+include("functions.jl")
+include("problem.jl")
+
+Q,q,a,b = problem_data()
+
+function dual_gradient(Q,q,a,b,gamma)
+    ITER = 20000
+    y_k = randn(length(a))
+    res = zeros(ITER)
+    L = norm(Q,2)
+    y_k1 = similar(y_k)
+    f_y = zeros(ITER)
+    f_x = zeros(ITER)
+    x_k = zeros(length(a),ITER)
+
+    for i = 1:ITER
+        y_k1 = -prox_boxconj(-(y_k - gamma*grad_quadconj(y_k,Q,q)), a, b, gamma)
+        res[i] = norm(y_k1 - y_k,2)
+        y_k = y_k1
+        f_y[i] = quadconj(y_k,Q,q)
+        x_k[:,i] = dual2primal(y_k,Q,q,a,b)
+        #println(y_k)
+        #println(x_k[:,i])
+        f_x[i] = quad(x_k[:,i],Q,q)
+
+    end
+
+    return y_k,res,f_x
+end
+
+function proxi_gradient(Q,q,a,b, gamma)
+    ITER = 20000
+    x_k = randn(length(a))
+    res = zeros(ITER)
+    x_k1 = similar(x_k)
+    f_x = zeros(ITER)
+
+    for i = 1:ITER
+        y_k = x_k - gamma*grad_quad(x_k,Q,q)
+        x_k1 = prox_box(y_k, a, b, gamma)
+        res[i] = norm(x_k1 - x_k,2)
+        x_k = x_k1
+        #println(x_k)
+        f_x[i] = quad(x_k, Q, q)
+    end
+
+    return x_k,res,f_x
+end
+
+function plot_prim(gamma = LinRange(0,2/norm(Q,2),10))
+    L = 2/norm(Q,2)
+    n_g = length(gamma)
+    res = zeros(20000,n_g)
+    x_star = zeros(length(a),n_g)
+    f_x = zeros(20000,n_g)
+    for i = 1:n_g
+        x_star[:,i],res[:,i],f_x[:,i] = proxi_gradient(Q,q,a,b,gamma[i])
+        #println("final f(x) : " , f_x[end,i])
+    end
+    #print(f_x)
+    plot!(x_star[:,4:end-2])
+    #plot(res)
+    return (x_star[:,4:end-2])
+end
+
+function plot_conj(gamma = LinRange(0,2/norm(inv(Q),2),10))
+    L = norm(inv(Q),2)
+    n_g = length(gamma)
+    res_conj = zeros(20000,n_g)
+    f_x = zeros(20000, n_g)
+    x_star = zeros(length(a),n_g)
+    for i = 1:n_g
+        y_star,res_conj[:,i],f_x[:,i] = dual_gradient(Q,q,a,b,gamma[i])
+        x_star[:,i] = dual2primal(y_star,Q,q,a,b)
+        #print("final f(x) = ", f_x[end,i])
+    end
+    #plot(f_x)
+    plot(x_star[:,4:end-2])
+    return (x_star[:,4:end-2])
+end
