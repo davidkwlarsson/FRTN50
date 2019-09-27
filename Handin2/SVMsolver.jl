55d5a0691368bbc8ef2b2813e3bae653c59ccd43
davidkwlarsson <dkw.larsson@gmail.com>
2019-09-27 10:21:17 +0200

    Fix everything

--- Handin2/SVMsolver.jl
+++ Handin2/SVMsolver.jl
@@ -3,10 +3,11 @@ using ProximalOperators, LinearAlgebra, Plots
 
 include("problem.jl")
 
-x_train, y_train = svm_train()
+
 x_test, y_test = svm_test_1()
-lambda = 0.01
+lambda = 0.001
 sigma = 0.5
+
 
 function GaussKernelMatrix(x)
     N = length(x)
@@ -25,21 +26,25 @@ end
 
 
 function prox_grad_method(x,y)
-    lambda = 0.01
-    ITER = 1
+    ITER = 10000
     K = GaussKernelMatrix(x)
     N = length(x)
-    Q = diagm(y)*K*diagm(y)
+    Q = 1/lambda * diagm(y)*K*diagm(y)
     gamma = 1/norm(Q,2)
-    h1 = Conjugate(HingeLoss(ones(N),N))
-    g = SqrNormL2(1/(2*lambda))
+    h1 = Conjugate(HingeLoss(ones(N),1/N))
+    # g = SqrNormL2(1/(2*lambda))
     dual = randn(N)
     res = zeros(ITER)
     for i = 1:ITER
-        grad_g = 1/lambda * Q * dual
+        grad_g = Q * dual
         y_k = dual - gamma*grad_g
         dual_1, hw =  prox(h1, y_k, gamma)
+        res = norm(dual .- dual_1, 2)
+        if i % 1000 == 0
+            #println(res)
+        end
         dual = dual_1
+
     end
     return dual
 end
@@ -55,15 +60,34 @@ function prediction(dual, x)
 end
 
 
-function testSVM()
+function testSVM(x_t, y_t)
     dual = prox_grad_method(x_train, y_train)
     y_pred = similar(y_test)
     for i = 1:length(x_test)
         y_pred[i] = prediction(dual, x_test[i,:])
     end
+    naive_class = 1
+    #naive_class = -1
+    naive_errors = sum(abs.(y_test.-naive_class))/2
     errors = sum(abs.(y_test.-y_pred))/2
-    print(errors, " errors out of ", length(x_test))
+    print(errors, " errors out of ", length(x_test),
+            ", naive classifier errors : " , naive_errors)
 
 end
 
+lambda = 0.01
+sigma = 0.5
+
+#configuration of (lambda, sigma) = (0.1, 2) (0.001, 0.5) (0.00001, 0.25)
+x_train, y_train = svm_train()
 testSVM()
+println("===============")
+x_train, y_train = svm_test_2()
+testSVM()
+println("===============")
+x_train, y_train = svm_test_3()
+testSVM()
+println("===============")
+x_train, y_train = svm_test_4()
+testSVM(x_train, y_train)
+println("\n")
